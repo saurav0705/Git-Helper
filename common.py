8c6092b8c1a82b46a93badb5812c5016479c3291
Saurav Aggarwal <saurav.aggarwal@flipkart.com>
2021-01-24 17:41:20 +0530

    added colored text function in common

--- common.py
+++ common.py
@@ -11,3 +11,15 @@ def execute_command(command):
 
 def converted_path(path):
     return path.replace('~', expanduser("~"))
+
+
+def print_in_color(msg, color='none', end='\n'):
+    color = color.upper()
+    if(color == 'GREEN'):
+        print("\033[92m {}\033[00m" .format(msg), end=end)
+    elif(color == 'RED'):
+        print("\033[91m {}\033[00m" .format(msg), end=end)
+    elif(color == 'ORANGE'):
+        print("\033[93m {}\033[00m" .format(msg), end=end)
+    else:
+        print("{}" .format(msg), end=end)
